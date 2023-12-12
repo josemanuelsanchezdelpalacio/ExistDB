4750180df87487b9e93924b9ed3a21c74bb2e590
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-12-12 11:16:53 +0100

    commit

--- src/main/java/Conexiones/ConexionExistDB.java
+++ src/main/java/Conexiones/ConexionExistDB.java
@@ -1,186 +1,67 @@
 package Conexiones;
 
-import org.exist.xmldb.EXistResource;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.*;
 import org.xmldb.api.modules.CollectionManagementService;
-import org.xmldb.api.modules.XQueryService;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.sql.Connection;
 
-/**********************************************************************************************************************************************
- *   APLICACIÓN: "JLC_SAM"                                                                                                                    *
- **********************************************************************************************************************************************
- *   ACCESO A DATOS - FACTORÍA DE PROYECTOS 2DAM  -  Intellij Ultimate IDE v2022.3.2                                                          *
- **********************************************************************************************************************************************
- *   @author Jorge Otin Caba, Laura Butler Gracia, Jose Manuel Sanchez del Palacio                  										  *
- *   @since 13-ENERO-2023                                                                                                                     *
- **********************************************************************************************************************************************
- *   COMENTARIOS:                                                                                                                             *
- *        - Clase ConexionExistDB.                                                                                                            *
- *        - Clase que contiene métodos para conectar con la base de datos eXist.                                                              *
- *        - Se ha utilizado la librería eXist-db-5.3.0.                                                                                       *
- *        - Se ha utilizado la librería exist-optional-5.3.0.                                                                                 *
- *        - Se ha utilizado la librería exist-core-5.3.0.                                                                                     *
- **********************************************************************************************************************************************/
 public class ConexionExistDB {
+
+    //Driver para eXist-DB
     private static String driver = "org.exist.xmldb.DatabaseImpl";
-    private static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc";
+    private static String URI = "xmldb:exist://localhost:8181";
     private static String usuario = "admin";
     private static String password = "";
 
+    //colección para almacenar las consultas sobre la colección referenciada con URI
+    static Collection col = null;
+
     //Conexión a la base de datos eXist
-    public static Connection conexionExistDb() throws ClassNotFoundException, XMLDBException, InstantiationException, IllegalAccessException {
-        //Registrar el driver
-        Class cl = Class.forName(driver);
-        Database database = (Database) cl.newInstance();
-        DatabaseManager.registerDatabase(database);
-
-        //Crear la colección
-        Collection col = DatabaseManager.getCollection(URI + "/db", usuario, password);
-        CollectionManagementService service = (CollectionManagementService) col.getService("CollectionManagementService", "1.0");
-        col = service.createCollection("centros");
-
-        //Crear el recurso
-        Resource res = col.createResource("proyectos.xml", "XMLResource");
-        Resource res1 = col.createResource("centrosFP2.xml", "XMLResource");
-        Resource res2 = col.createResource("familias.xml", "XMLResource");
-
-        //Asignar el recurso a un archivo XML
-        File file = new File("target/proyectos.xml");
-        File file1 = new File("target/centrosFP2.xml");
-        File file2 = new File("target/familias.xml");
-        res.setContent(file);
-        res1.setContent(file1);
-        res2.setContent(file2);
-        System.out.println("Subido el archivo XML a la base de datos eXist");
-
-        //Guardar el recurso
-        col.storeResource(res);
-        col.storeResource(res1);
-        col.storeResource(res2);
-        System.out.println("Guardado el recurso");
-
-        //Cerrar la conexión
-        col.close();
-        System.out.println("Cerrada la conexión");
-        return null;
-    }
-
-    public static void consultaExist() throws ClassNotFoundException, InstantiationException, IllegalAccessException, XMLDBException, IOException {
-        final String driver = "org.exist.xmldb.DatabaseImpl";
-
-        // Inicializar el driver
-        Class cl = Class.forName(driver);
-        Database database = (Database) cl.newInstance();
-        database.setProperty("create-database", "true");
-        DatabaseManager.registerDatabase(database);
-
-        Collection col = null;
-        // Obtener la colección
+    public static void conexionExistDb() {
+        //Carga del driver
+        Class cl = null;
+        //Instancia de la BD
+        Database database = null;
         try {
-            col = DatabaseManager.getCollection("xmldb:exist://localhost:8080/exist/xmlrpc/db/");
-            XQueryService xqs = (XQueryService) col.getService("XQueryService", "1.0");
-            xqs.setProperty("indent", "yes");
+            cl = Class.forName(driver);
+            database = (Database) cl.newInstance();
+            //Registro del driver
+            DatabaseManager.registerDatabase(database);
 
-            //Consulta para obtener los centros
-            ResourceSet resultCentros = xqs.query("<entidades>\n" +
-                    "{\n" +
-                    "let $x := doc(\"centros/centrosFP2.xml\")//*:root/worksheet/Row\n" +
-                    "return \n" +
-                    "    for $y in $x\n" +
-                    "    return\n" +
-                    "    <entidad>\n" +
-                    "        <CODIGO>{$y/CODIGOCENTRO/text()}</CODIGO>\n" +
-                    "        <NOMBRE>{$y/NOMBRECENTRO/text()}</NOMBRE>\n" +
-                    "        <ID_PROFESION>1</ID_PROFESION>\n" +
-                    "        <WEB>-</WEB>\n" +
-                    "        <CONTACTO>-</CONTACTO>\n" +
-                    "    </entidad>\n" +
-                    "}\n" +
-                    "</entidades>\n");
+            //conexion a la coleccion
+            col = DatabaseManager.getCollection(URI + "/db", usuario, password);
+            CollectionManagementService service = (CollectionManagementService) col.getService("CollectionManagementService", "1.0");
+            col = service.createCollection("xml");
 
-            //Consulta para obtener los proyectos
-            ResourceSet resultProyecto = xqs.query("<proyectos>\n" +
-                    "{\n" +
-                    "let $x := doc(\"centros/proyectos.xml\")//*:root/worksheet/Row\n" +
-                    "return \n" +
-                    "    for $y in $x\n" +
-                    "    return\n" +
-                    "    <proyecto>\n" +
-                    "        <TITULO>{$y/TÍTULODELPROYECTO/text()}</TITULO>\n" +
-                    "        <DESCRIPCION>-</DESCRIPCION>\n" +
-                    "        <ID_TAGS>1</ID_TAGS>\n" +
-                    "        <ID_PROFESION></ID_PROFESION>\n" +
-                    "        <VALORACION>POSITIVO</VALORACION>\n" +
-                    "        <VISIBILIDAD>true</VISIBILIDAD>\n" +
-                    "        <ID_ESTADO>1</ID_ESTADO>\n" +
-                    "        <FECHAINICIO>{$y/fn:substring-after(AUTORIZACIÓN, 'Resolución ')}</FECHAINICIO>\n" +
-                    "        <FECHAFIN>{$y/fn:substring-after(CONTINUIDAD, 'Resolución ')}</FECHAFIN>\n" +
-                    "    </proyecto>\n" +
-                    "}\n" +
-                    "</proyectos>\n");
+            //Crear el recurso
+            Resource res = col.createResource("familias.xml", "XMLResource");
+            Resource res1 = col.createResource("proyectosFP.xml", "XMLResource");
 
-            //Consulta para obtener las familias profesionales
-            ResourceSet resultFamilia = xqs.query("<familias_profesionales>\n" +
-                    "{\n" +
-                    "let $x := doc(\"centros/familias.xml\")//*:options/option\n" +
-                    "return \n" +
-                    "    for $y in $x\n" +
-                    "    return\n" +
-                    "    <familia_profesional>" +
-                    "    <nombre>{$y/text()}</nombre>" +
-                    "    </familia_profesional>\n" +
-                    "}\n" +
-                    "</familias_profesionales>\n");
+            //Asignar el recurso a un archivo XML
+            File file = new File("src/main/resources/familias.xml");
+            File file1 = new File("src/main/resources/proyectosFP.xml");
+            res.setContent(file);
+            res1.setContent(file1);
+            System.out.println("Subido los archivos XML a la base de datos eXist");
 
-            //Obtener los resultados
-            ResourceIterator i = resultCentros.getIterator();
-            ResourceIterator i1 = resultProyecto.getIterator();
-            ResourceIterator i2 = resultFamilia.getIterator();
-            Resource res = null;
-            Resource res1 = null;
-            Resource res2 = null;
+            //Guardar el recurso
+            col.storeResource(res);
+            col.storeResource(res1);
+            System.out.println("Guardado el recurso");
 
-            //Escribir los resultados en un archivo XML
-            while (i.hasMoreResources()) {
-                try {
-                    res = i.nextResource();
-                    res1 = i1.nextResource();
-                    res2 = i2.nextResource();
-                    FileWriter fw = new FileWriter("target/centrosFinal.xml");
-                    FileWriter fw1 = new FileWriter("target/proyectosFinal.xml");
-                    FileWriter fw2 = new FileWriter("target/familiasFinal.xml");
-                    fw.write(res.getContent().toString());
-                    fw1.write(res1.getContent().toString());
-                    fw2.write(res2.getContent().toString());
-                    fw.close();
-                    fw1.close();
-                    fw2.close();
-                    System.out.println("Consulta realizada correctamente");
-                } finally {
-                    //dont forget to cleanup resources
-                    try {
-                        ((EXistResource) res).freeResources();
-                        ((EXistResource) res1).freeResources();
-                        ((EXistResource) res2).freeResources();
-                    } catch (XMLDBException xe) {
-                        System.out.println("Error al liberar los recursos");
-                    }
-                }
-            }
-        } finally {
+            //Cerrar la conexión
+            col.close();
+            System.out.println("Cerrada la conexión");
 
-            if (col != null) {
-                try {
-                    col.close();
-                } catch (XMLDBException xe) {
-                    System.out.println("Error al cerrar la conexión");
-                }
-            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver no encontrado");
+        } catch (InstantiationException e) {
+            System.out.println("Error de instancia de BD");
+        } catch (IllegalAccessException e) {
+            System.out.println("Error al acceder a la base de datos");
+        } catch (XMLDBException e) {
+            System.out.println("Error al registrar el driver");
         }
     }
 }
--- src/main/java/FP2/ManejoBD.java
+++ src/main/java/FP2/ManejoBD.java
@@ -1,11 +1,10 @@
 package FP2;
 
-import Conexiones.ConexionMySQL;
 import Conexiones.ConexionExistDB;
+import libs.Leer;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class ManejoBD {
 
@@ -13,104 +12,23 @@ public class ManejoBD {
 
         boolean salir = false;
         int opcion;
+        do {
+            System.out.println("1. Conexión a existDB.");
+            System.out.println("0. Salir");
 
-        try (Connection conexion = ConexionMySQL.handleBD()) {
+            opcion = Leer.pedirEntero("Introduce una opción: ");
 
-            if (conexion != null) System.out.println("conexion establecida");
-
-            while (!salir) {
-
-                System.out.println("1. Crear BD");
-                System.out.println("2. Borrar BD");
-                System.out.println("3. Crear tablas");
-                System.out.println("4. Transformar de csv a xml.");
-                System.out.println("5. Conexión a existDB.");
-                System.out.println("6. Exportar datos XQuery a XML.");
-                System.out.println("7. Importar datos XML a la base de datos.");
-                System.out.println("0. Salir");
-
-                try {
-                    opcion = Clases.LeerDatos.pideInt("");
-                } catch (NumberFormatException e) {
-                    System.out.println("Ingrese un número válido");
-                    continue;
-                }
-
-                switch (opcion) {
-                    case 0:
-                        salir = true;
-                        break;
-                    case 1:
-                        crearBD();
-                        break;
-                    case 2:
-                        borrarBD();
-                        break;
-                    case 3:
-                        CreacionTablas.crear();
-                        System.out.println("Tablas creadas");
-                        break;
-                    case 4:
-                        Transformaciones.TransformarCsvXml.transformarCsvXml();
-                        System.out.println("Conversión realizada.");
-                        break;
-                    case 5:
-                        ConexionExistDB.conexionExistDb();
-                        break;
-                    case 6:
-                        ConexionExistDB.consultaExist();
-                        break;
-                    case 7:
-                        Transformaciones.TransformarXMLEstados.transformarXmlEstados();
-                        System.out.println("Estados importados");
-                        Transformaciones.TransformarXMLRoles.transformarXmlRoles();
-                        System.out.println("Roles importados");
-                        Transformaciones.TransformarXMLTags.transformarXmlTags();
-                        System.out.println("Tags importados");
-                        Transformaciones.TransformarXMLProfesiones.transformarXmlProfesiones();
-                        System.out.println("Profesiones importadas");
-                        Transformaciones.TransformarXMLEntidades.transformarXmlEntidades();
-                        System.out.println("Entidades importadas");
-                        Transformaciones.TransformarXMLProyectos.transformarXmlProyectos();
-                        System.out.println("Proyectos importados");
-                        Transformaciones.TransformarXMLUsuarios.transformarXmlUsuarios();
-                        System.out.println("Usuarios importados");
-                        Transformaciones.TransformarXMLParticipaciones.transformarXmlParticipaciones();
-                        System.out.println("Participaciones importadas");
-                        break;
-                    default:
-                        System.out.println("Ingrese una opción válida");
-                }
+            switch (opcion) {
+                case 0:
+                    salir = true;
+                    break;
+                case 1:
+                    ConexionExistDB.conexionExistDb();
+                    break;
+                default:
+                    System.out.println("La opcion seleccionada no existe");
             }
 
-        } catch (SQLException e) {
-            System.out.println("Error al conectar con la base de datos: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Ocurrió un error inesperado: " + e.getMessage());
-        }
-    }
-
-
-    // Crear y borrar base de datos
-    static void crearBD() {
-        try (Connection con = ConexionMySQL.handleBD()) {
-            try (Statement stmt = con.createStatement()) {
-                stmt.execute("CREATE DATABASE BdFactoriaProyectos");
-                System.out.println("BD creada");
-            }
-        } catch (SQLException e) {
-            System.out.println("La base de datos ya existe.");
-        }
-    }
-
-    static void borrarBD() {
-        try (Connection con = ConexionMySQL.conectar()) {
-            try (Statement stmt = con.createStatement()) {
-                stmt.execute("DROP DATABASE BdFactoriaProyectos");
-                System.out.println("BD borrada");
-            }
-        } catch (SQLException e) {
-            System.out.println("La base de datos no existe.");
-        }
+        } while (!salir);
     }
 }
