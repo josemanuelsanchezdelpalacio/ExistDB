09bfacb95558ef2031485569bc0191fd6febb04a
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-12-12 17:39:24 +0100

    commit

--- src/main/java/Conexiones/ConexionExistDB.java
+++ src/main/java/Conexiones/ConexionExistDB.java
@@ -3,58 +3,59 @@ package Conexiones;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.*;
 import org.xmldb.api.modules.CollectionManagementService;
+import org.xmldb.api.modules.XMLResource;
 
 import java.io.File;
 
 public class ConexionExistDB {
 
-    //Driver para eXist-DB
     private static String driver = "org.exist.xmldb.DatabaseImpl";
-    private static String URI = "xmldb:exist://localhost:8181/exist/xmlrpc/db";
+    private static String URI = "xmldb:exist://localhost:8181/exist/xmlrpc/db/coleccionXML";
     private static String usuario = "admin";
     private static String password = "";
 
-    //colección para almacenar las consultas sobre la colección referenciada con URI
     static Collection col = null;
 
-    //Conexión a la base de datos eXist
     public static void conexionExistDb() {
-        //Carga del driver
-        Class cl = null;
-        //Instancia de la BD
-        Database database = null;
         try {
-            cl = Class.forName(driver);
-            database = (Database) cl.newInstance();
-            //Registro del driver
+            // Carga del driver
+            Class cl = Class.forName(driver);
+            //instancia de la base de datos
+            Database database = (Database) cl.newInstance();
+            //registro del driver
             DatabaseManager.registerDatabase(database);
 
             //conexion a la coleccion
-            col = DatabaseManager.getCollection(URI + "/db", usuario, password);
+            col = DatabaseManager.getCollection(URI, usuario, password);
             if (col == null) {
-                System.out.println("Error al obtener la colección");
-                return;
+                // Si no existe, la creamos
+                CollectionManagementService service = (CollectionManagementService) col.getService("CollectionManagementService", "1.0");
+                service.createCollection("coleccionXML");
             }
 
-            CollectionManagementService service = (CollectionManagementService) col.getService("CollectionManagementService", "1.0");
 
-            //Crear el recurso
-            Resource res = col.createResource("familias.xml", "XMLResource");
-            Resource res1 = col.createResource("proyectosFP.xml", "XMLResource");
-
-            //Asignar el recurso a un archivo XML
+            // Crear el recurso
             File file = new File("src/main/resources/familias.xml");
-            File file1 = new File("src/main/resources/proyectosFP.xml");
-            res.setContent(file);
-            res1.setContent(file1);
-            System.out.println("Subido los archivos XML a la base de datos eXist");
+            File file2 = new File("src/main/resources/proyectosFP.xml");
+            if (!file.canRead() || !file2.canRead()) {
+                System.out.println("Error al leer el documento XML.");
+            } else {
+                // comprobar si es un archivo
+                Resource resource = col.createResource(file.getName(), "XMLResource");
+                Resource resource2 = col.createResource(file2.getName(), "XMLResource");
 
-            //Guardar el recurso
-            col.storeResource(res);
-            col.storeResource(res1);
-            System.out.println("Guardado el recurso");
+                // Asignar el recurso a un archivo XML
+                resource.setContent(file);
+                resource2.setContent(file2);
+                System.out.println("Subido los archivos XML a la base de datos eXist");
 
-            //Cerrar la conexión
+                // Guardar el recurso
+                col.storeResource(resource);
+                col.storeResource(resource2);
+                System.out.println("Guardado el recurso");
+            }
+
+            // Cerrar la conexión
             col.close();
             System.out.println("Cerrada la conexión");
 
